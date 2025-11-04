42246bbeb975423eb71e3e2a9ce8a9ece475c091
Tanathorn-Rin <so.tanathorn@gmail.com>
2025-11-04 23:34:59 +0700

    Add Fetching Data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState([]);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('http://localhost:5000/api/workouts');
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        }
+
+        fetchWorkouts();
+    }, []);
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <div className="workout-details" key={workout._id}>
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Reps: </strong>{workout.reps}</p>
+            <p>{new Date(workout.createdAt).toLocaleDateString('en-US')}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
